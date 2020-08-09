f93abe818813000e148b90a2c21fafa7be8532cd
t-ambur <trevoramburgey@gmail.com>
2020-08-09 12:41:06 -0400

    Updates

---
+++ precache-manifest.6cbba781386a6fbfce97264e64626f11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43310cd7ca5e99d06ead91dc81681b91",
+    "url": "/CompanionForGrimDawn_v2/index.html"
+  },
+  {
+    "revision": "9c930952edd4299fed69",
+    "url": "/CompanionForGrimDawn_v2/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "783a7a3d9a4e7a03ba4b",
+    "url": "/CompanionForGrimDawn_v2/static/js/2.fcb2b400.chunk.js"
+  },
+  {
+    "revision": "9c930952edd4299fed69",
+    "url": "/CompanionForGrimDawn_v2/static/js/main.f6af6d9a.chunk.js"
+  },
+  {
+    "revision": "c2f93e15b738f6cc9761",
+    "url": "/CompanionForGrimDawn_v2/static/js/runtime-main.c9fc97f8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/CompanionForGrimDawn_v2/precache-manifest.6cbba781386a6fbfce97264e64626f11.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CompanionForGrimDawn_v2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
