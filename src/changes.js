0e3922500f83a5b5e97b70e37804a7fca4e6f5b9
t-ambur <trevoramburgey@gmail.com>
2020-08-11 21:32:41 -0400

    changelog update

--- src/changes.js
+++ src/changes.js
@@ -17,11 +17,18 @@ class Changes extends React.Component
     render() {
         return(
             <div>
+                <p>
+                    If you are having trouble seeing an update listed in this log: clear your cookies and cache for this webpage and refresh the page.
+                    <br/>
+                    You will always see the latest version if you set your browser to delete cookies for this webpage on browser close.
+                </p>
                 <h1> Change Log </h1>
 
-                <h3> 08/10/2020 - v0.2.0 </h3>
+                <h3> 08/11/2020 - v0.2.0 </h3>
                 <ul>
-                    <li> WIP - Tier 1 Devotions up to date with Grim Dawn version v1.1.7.2</li>
+                    <li> All Devotion Descriptions up to date with Grim Dawn version v1.1.7.2</li>
+                    <li> All Devotion Descriptions now show the node sequence when adding points to them</li>
+                    <li> All Devotion Descriptions are reformatted </li>
                     <li> Forked and rehosted from https://github.com/andrewsnyder328/CompanionForGrimDawn to https://github.com/t-ambur/CompanionForGrimDawn_v2 </li>
                     <li> Added simple navigation bar/buttons at top of screen (Devotions, Masteries, Links, Change Log) </li>
                     <li> Added some important links</li>
